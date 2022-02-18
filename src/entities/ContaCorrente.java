72db474c14868966c9acf5a5ce91d2e982058ee8
EdsonCampolina <ejcsengsoft@gmail.com>
2022-02-18 19:57:29 -0300

    Abstração do projeto

---
+++ src/entities/ContaCorrente.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class ContaCorrente {
+
+	private Integer agencia;
+	private Integer numero;
+	private Double saldo;
+
+	public void sacar() {
+	}
+
+	public void depositar() {
+	}
+
+	public void transferir() {
+	}
+}
---
+++ src/entities/ContaPoupanca.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class ContaPoupanca {
+
+	private Integer agencia;
+	private Integer numero;
+	private Double saldo;
+
+	public void sacar() {
+	}
+
+	public void depositar() {
+	}
+
+	public void transferir() {
+	}
+}
