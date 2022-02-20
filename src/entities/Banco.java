d909fa6571113ee686a3849b8c1c65350671e8bb
EdsonCampolina <ejcsengsoft@gmail.com>
2022-02-20 01:59:10 -0300

    Projeto finalizado

--- src/entities/Banco.java
+++ src/entities/Banco.java
@@ -1,12 +1,9 @@
 package entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Banco {
 
 	private String nome;
-	private List<Conta> contas = new ArrayList<>();
+	// private List<Conta> contas = new ArrayList<>();
 
 	public String getNome() {
 		return nome;
